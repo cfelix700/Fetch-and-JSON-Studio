50665276dafe6f242ff144757ec5b6a4efe2703e
Christian Felix <christianfelix@Chris-Felixs-MacBook-Pro.local>
2022-11-28 20:07:11 -0600

    message

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// TODO: add code here
+
+
+window.addEventListener("load", function() {
+    const container = document.getElementById("container");
+    
+    
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+        .then( function(response){ return response.json() })
+        .then( function(json){
+            console.log(json);
+
+        for (let i = 0; i<json.length; i++) {
+                container.innerHTML += `
+                <div class = "astronaut">
+                     <div class = "bio">
+                <h3>Astronaut's Name: ${json[i].firstName} ${json[i].lastName}</h3>
+                <ul>
+                    <li>Hours in space: ${json[i].hoursInSpace}</li>
+                    <li>Active: ${json[i].active}</li>
+                    <li>Skills: ${json[i].skills}</li>
+                <ul>   
+            </div>
+            <img class="avatar" src=${json[i].picture}>
+            </div>
+            `;
+
+        }
+            
+        });  
+    })
